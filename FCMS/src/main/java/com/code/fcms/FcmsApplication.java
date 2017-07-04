27eb0a6ddc8de81841e5d7330ed49fbf7d282e99
Lucas Albuquerque de Almeida Andrade <lucas.almeida@ccc.ufcg.edu.br>
2017-07-04 11:55:09 -0300

    Setuping project structure

---
+++ FCMS/src/main/java/com/code/fcms/FcmsApplication.java
@@ -0,0 +1,12 @@
+package com.code.fcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcmsApplication.class, args);
+	}
+}
